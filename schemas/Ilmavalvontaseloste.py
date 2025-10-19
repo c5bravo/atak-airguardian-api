c45d48d25cb81dc806c5ce9743e3bc10c4423a01
Aamunen <228656696+Aamunen@users.noreply.github.com>
2025-10-19 12:15:36 +0300

    rename desc for direction

--- schemas/Ilmavalvontaseloste.py
+++ schemas/Ilmavalvontaseloste.py
@@ -10,7 +10,7 @@ class AirSurveillanceLog(BaseModel):
     aircraft_id: str = Field(description="Indentification number from the aircraft. Str because id can start with 0")
     general_position: str = Field(description="Two Nato-alphabethical letters")
     accurate_poisition: str(max_length=2) = Field(description="Position defined within 10km")
-    speed: Literal("slow" | "fast" | "supersonic") = Field(description="Speed of the air unit")
-    direction: int = Field(description="Speed of the air unit")
+    speed: Literal("slow" | "fast" | "supersonic") = Field(description="Speed of the aricraft")
+    direction: int = Field(description="Direction of aricraft")
     altitude: Literal("surface" | "low" | "high") = Field
     details: Optional[str] = Field(description="Additional info", default=None)
