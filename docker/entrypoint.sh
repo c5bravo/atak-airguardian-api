d346555866d229f24f0d5e7da9a6aeadd45e92d2
Eero af Heurlin <eero.afheurlin@iki.fi>
2025-11-27 10:15:16 +0200

    chore: add some basic qa-things and docker entrypoint scripts (the Dockerfile must be refactored for multi-stage builds and to use entrypoints

---
+++ docker/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+# shellcheck disable=SC1091
+. /container-init.sh
+
+set -e
+if [ "$#" -eq 0 ]; then
+  # FIXME: can we know the traefik/nginx internal docker ip easily ?
+  exec gunicorn "test-radar.main.app" --bind 0.0.0.0:8005 --forwarded-allow-ips='*' -w 4 -k uvicorn.workers.UvicornWorker
+else
+  exec "$@"
+fi
