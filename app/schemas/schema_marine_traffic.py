2daab33216e4fd3077b27c05a7e078b4bd253e40
LightningEagle2 <EXT-Abdulhadi.Labanie@cloud.fdf.fi>
2026-01-26 15:40:38 +0200

    adding: new column (type) for marine

--- app/schemas/schema_marine_traffic.py
+++ app/schemas/schema_marine_traffic.py
@@ -4,15 +4,14 @@ from datetime import datetime
 
 
 class MarineTrafficPosition(BaseModel):
-    MMSI: Optional[str]
-    IMO: Optional[str]
     SHIP_ID: str
     LAT: float
     LON: float
-    SPEED: float
-    HEADING: int
-    COURSE: Optional[str]
-    TIMESTAMP: datetime
-    UTC_SECONDS: Optional[str]
-    SHIPNAME: str
-    SHIP_COUNTRY: str
+    SPEED: Optional[float]
+    HEADING: Optional[int]
+    COURSE: Optional[int]
+    TIMESTAMP: Optional[datetime]
+    UTC_SECONDS: Optional[int]
+    SHIPNAME: Optional[str]
+    SHIP_COUNTRY: Optional[str]
+    type: Optional[str]
