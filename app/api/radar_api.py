85b5d289ded330a0310c98577b68ed70988419ed
LightningEagle2 <EXT-Abdulhadi.Labanie@cloud.fdf.fi>
2026-01-26 15:44:02 +0200

    adding: transform format for practice in api

--- app/api/radar_api.py
+++ app/api/radar_api.py
@@ -1,12 +1,14 @@
 import logging
 from datetime import datetime
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, cast
 
 import mgrs  # type: ignore
 from fastapi import APIRouter, HTTPException
 
 from app.schemas.schema import TransformedAircraft
+from app.schemas.schema_marine_traffic import MarineTrafficPosition
 from app.tasks.practice_task import fetch_practice_data
+from app.tasks.marine_traffic_task import fetch_marine_traffic_data
 # from app.tasks.radar_task import fetch_aircraft_data
 
 logger = logging.getLogger(__name__)
@@ -95,6 +97,7 @@ def transform_aircraft(aircraft: Dict[str, Any]) -> TransformedAircraft:
         "direction": true_track,
         "details": more_details(aircraft),
         "isExited": bool(aircraft.get("isExited")),
+        "type": "openSky",
     }
     return transformed
 
@@ -103,6 +106,32 @@ def filter_on_ground(aircraft: Dict[str, Any]) -> bool:
     # Only aircraft not on ground
     return not bool(aircraft.get("on_ground"))
 
+def transform_practice(aircraft_pc: Dict[str, Any]) -> TransformedAircraft:
+    return cast(TransformedAircraft, {
+        "id": 0,
+        "aircraftId": aircraft_pc.get("callsign"),
+        "position": aircraft_pc.get("position"),
+        "altitude": aircraft_pc.get("altitude"),
+        "speed": aircraft_pc.get("velocity"),
+        "direction": aircraft_pc.get("direction") or 0,
+        "details": aircraft_pc.get("details"),
+        "isExited": bool(aircraft_pc.get("isExited")),
+        "type": "practiceTool",
+    })
+
+
+def transform_ship(ship: MarineTrafficPosition) -> TransformedAircraft:
+    return cast(TransformedAircraft, {
+        "id": 0,
+        "aircraftId": ship.SHIP_ID,
+        "position": convert_to_mgrs(ship.LON, ship.LAT),
+        "altitude": "surface",
+        "speed": "slow",
+        "direction": ship.HEADING or 0,
+        "details": f"Ship {ship.SHIPNAME} from {ship.SHIP_COUNTRY}",
+        "isExited": False,
+        "type": "marine",
+    })
 
 @router.get("/aircraft")
 def get_aircraft_data() -> List[TransformedAircraft]:
@@ -112,10 +141,20 @@ def get_aircraft_data() -> List[TransformedAircraft]:
         # filtered_data = list(filter(filter_on_ground, data))
         # transformed_aircraft = [transform_aircraft(ac) for ac in filtered_data]
 
+        marine_data = fetch_marine_traffic_data()
         practice_data = fetch_practice_data()
 
-        # return [*transformed_aircraft, *practice_data]
-        return [*practice_data]
+        # Assume practice_data already transformed
+        transformed_practice = practice_data  
+
+        # Transform marine ship data
+        transformed_ships = [transform_ship(ship) for ship in marine_data]
+
+        return [*transformed_practice, *transformed_ships]
+    
+        # --- for useing OpenSky data switch add transformed_aircraft for return ---
+        # return [*transformed_practice, *transformed_ships, *transformed_aircraft]
+
     except Exception as e:
         logger.error(f"Error retrieving aircraft data: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
